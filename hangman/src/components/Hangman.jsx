0a3ea04e45355decd4ec302b79eaf295db350281
flportilla <flportilla@gmail.com>
2022-06-16 17:05:13 -0500

    Add man

--- hangman/src/components/Hangman.jsx
+++ hangman/src/components/Hangman.jsx
@@ -3,7 +3,16 @@ import '../styles/Hangman.css'
 function Hangman() {
   return (
     <>
-      <div className='gallow'></div>
+      <div className='gallow'>
+        <div className="man_container">
+          <div className="head"></div>
+          <div className="torso"></div>
+          <div className="left_arm"></div>
+          <div className="right_arm"></div>
+          <div className="left_leg"></div>
+          <div className="right_leg"></div>
+        </div>
+      </div>
     </>
   )
 }
