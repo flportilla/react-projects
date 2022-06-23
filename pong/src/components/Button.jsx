14304dc29413b10738c02602e0cf32ef2d577382
flportilla <flportilla@gmail.com>
2022-06-23 11:40:48 -0500

    Draw one not moving ball

--- pong/src/components/Button.jsx
+++ pong/src/components/Button.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import '../styles/Button.css'
 
-function Button({ fade }) {
+function Button({ fade, context, canvas }) {
+
+  const getContext = () => context()
+  const getCanvas = () => canvas()
+
+  //Get initial X and Y position for the ball
+  let xPos = getCanvas().width / 2
+  let yPos = Math.floor(Math.random() * (getCanvas().height - 25))
+
+  let dx = 2;
+  let dy = -2
+
+  function drawBall() {
+
+
+    //draw the ball
+    getContext().beginPath();
+    getContext().arc(xPos, yPos, 15, 0, 2 * Math.PI);
+    getContext().fillStyle = 'red';
+    getContext().fill();
+    getContext().stroke();
+  }
+
+  function draw() {
+    getContext().clearRect(0, 0, getCanvas().width, getCanvas().height);
+    drawBall();
+    xPos += dx;
+    yPos += dy;
+  }
+
 
   function handleClicks() {
+    //Hide instructions
     fade();
+    drawBall();
+
   }
 
   return (
