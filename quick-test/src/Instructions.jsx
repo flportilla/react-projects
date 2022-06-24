c02eaaa7ed043dce6fffb3834c7b1c7b7f4c92d5
flportilla <flportilla@gmail.com>
2022-06-24 13:42:16 -0500

    Add css to instructions modal

--- quick-test/src/Instructions.jsx
+++ quick-test/src/Instructions.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Instructions.css'
 
 function Instructions(props) {
   return (
-    <div>Instructions</div>
+    <>
+      <div className="instructions">
+        <h1>Typing Test</h1>
+        <h2>How many words per minute you can type?</h2>
+        <p>Hit start to get your test passage of text and begin typing. You'll have 60 seconds to type as much of the passage as you can. The timer will start when you begin typing. Good luck!</p>
+        <button className="btn">Start</button>
+      </div>
+    </>
   )
 }
 
