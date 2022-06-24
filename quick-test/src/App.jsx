7fab21a6ebe340e6c2207e08b032b5b39500ea05
flportilla <flportilla@gmail.com>
2022-06-24 13:30:22 -0500

    Create first Instructions basic file component

---
+++ quick-test/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Instructions from './Instructions';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="instructions_container">
+        <Instructions />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ quick-test/src/Instructions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Instructions(props) {
+  return (
+    <div>Instructions</div>
+  )
+}
+
+export default Instructions
