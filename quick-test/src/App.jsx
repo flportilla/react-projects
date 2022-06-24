bb70c675225bad3a4fdbebdef11ffcea761cf5c4
flportilla <flportilla@gmail.com>
2022-06-24 13:56:50 -0500

    Add logic to hide instructions

--- quick-test/src/App.jsx
+++ quick-test/src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css';
+import { useEffect, useRef } from 'react';
 import Instructions from './Instructions';
 
+
 function App() {
+
+
+  const instructionsRef = useRef(null)
+  useEffect(() => { instructionsRef.current.focus() })
+  const hide = () => instructionsRef.current.classList.add('hide')
+
+
   return (
     <div className="App">
-      <div className="instructions_container">
-        <Instructions />
+      <div
+        onTransitionEnd={hide}
+        ref={instructionsRef}
+        className="instructions_container"
+      >
+        <Instructions
+          instructionsRef={instructionsRef} />
+      </div>
+      <div className='game_container'>
+
       </div>
 
     </div>
--- quick-test/src/Instructions.jsx
+++ quick-test/src/Instructions.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './Instructions.css'
 
 function Instructions(props) {
+
+  const fade = () => props.instructionsRef.current.classList.add('fade')
+
   return (
     <>
       <div className="instructions">
         <h1>Typing Test</h1>
         <h2>How many words per minute you can type?</h2>
         <p>Hit start to get your test passage of text and begin typing. You'll have 60 seconds to type as much of the passage as you can. The timer will start when you begin typing. Good luck!</p>
-        <button className="btn">Start</button>
+        <button onClick={fade} className="btn">Start</button>
       </div>
     </>
   )
