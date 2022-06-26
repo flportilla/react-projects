405b5c9d73c76f33520a0ade554246b856399042
flportilla <flportilla@gmail.com>
2022-06-26 07:28:27 -0500

    Remove unused files and create basic canva

--- garden/src/App.js
+++ garden/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const canvasStyle = {
+    height: Number(`${window.innerHeight}`),
+    width: Number(`${window.innerWidth}`),
+  };
+
+
+  function randomColor() {
+    return ('#' + Math.floor(Math.random() * 16777215).toString(16));
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        width={canvasStyle.width}
+        height={canvasStyle.height}
+        style={{ backgroundColor: '#252525' }}
+      />
+
     </div>
   );
 }
