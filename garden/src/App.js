eed3addbc19928bb3449b662f5e12e2d45fb69dd
flportilla <flportilla@gmail.com>
2022-06-26 18:08:26 -0500

    Add logic to detect collision

--- garden/src/App.js
+++ garden/src/App.js
@@ -3,20 +3,32 @@ import Canvas from './Canvas';
 
 function App() {
 
+  const balls = []
+
   const canvasStyle = {
     height: Number(`${window.innerHeight}`),
     width: Number(`${window.innerWidth}`),
   };
 
-
   function randomColor() {
     return ('#' + Math.floor(Math.random() * 16777215).toString(16));
   }
 
+  function handleClick(e) {
+    const ctx = e.target.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI)
+    ctx.fillStyle = randomColor();
+    ctx.fill();
+    balls.push({ x: e.clientX, y: e.clientY, color: 'red' })
+
+  }
 
   return (
     <div className="App">
-      <Canvas style={canvasStyle} />
+      <canvas width={canvasStyle.width} height={canvasStyle.height} className='topCanvas' onClick={handleClick} />
+      <Canvas randomColor={randomColor} balls={balls} style={canvasStyle} />
+
     </div>
   );
 }
--- garden/src/Canvas.jsx
+++ garden/src/Canvas.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 function Canvas(props) {
 
+  const canvasStyle = {
+    height: Number(`${window.innerHeight}`),
+    width: Number(`${window.innerWidth}`),
+  };
+
+  function drawBigCircle(ctx, frameCount) {
+    ctx.clearRect(0, 0, canvasStyle.width, canvasStyle.height);
+    ctx.beginPath();
+    ctx.arc(ctx.canvas.width / 2, ctx.canvas.height / 2, frameCount, 0, 2 * Math.PI)
+    ctx.stroke();
+
+    props.balls.forEach(ball => {
+      ctx.beginPath();
+      ctx.arc(ball.x, ball.y, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+
+      if (Math.sqrt(Math.pow(ball.x - ctx.canvas.width / 2, 2) + Math.pow(ball.y - ctx.canvas.height / 2, 2)) < frameCount) {
+        console.log('touching');
+      }
+
+    })
+
+  }
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+
+
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    let frameCount = 0
+    let animationFrameId
+
+    const render = () => {
+      frameCount++
+      drawBigCircle(context, frameCount)
+      if (frameCount > canvasStyle.width / 2) {
+        frameCount = 0;
+      }
+      animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [drawBigCircle])
+
   return (
     <canvas
-      width={props.style.width}
-      height={props.style.height}
+      ref={canvasRef}
+      width={canvasStyle.width}
+      height={canvasStyle.height}
       style={{ backgroundColor: '#252525' }}
     > </canvas>
 
