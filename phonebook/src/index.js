89999e450b3706d528aa67f649244bb01ac0dc50
flportilla <flportilla@gmail.com>
2022-07-01 14:55:33 -0500

    Rearrange files

--- phonebook/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-// @ts-ignore
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
